3829758debedbb8ce2c0de99f58641f5461ce381
Ashal aka JOKER <ahastudio@gmail.com>
2019-12-14 11:38:22 +0900

    Track scroll action
    Add a new event `scroll` when current user scroll down the page.
    
    See also:
    - https://developer.mozilla.org/docs/Web/API/EventTarget/addEventListener
    - https://developer.mozilla.org/docs/Web/API/EventTarget/removeEventListener

--- src/google-analytics.js
+++ src/google-analytics.js
@@ -3,3 +3,16 @@ gtag('event', 'visit');
 document.querySelector('.subscribe form').addEventListener('submit', () => {
     gtag('event', 'subscribe');
 });
+
+(() => {
+  const handleScroll = () => {
+    if (window.scrollY < window.innerHeight / 2) {
+      return;
+    }
+
+    gtag('event', 'scroll');
+    window.removeEventListener('scroll', handleScroll);
+  };
+
+  window.addEventListener('scroll', handleScroll);
+})();
