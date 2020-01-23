8cc86b1d9e037eab6c08e6a484ef6b8b6be1364b
noah <noahlog@naver.com>
2020-01-23 14:10:40 +0900

    Show slide images on subscription insurance page
    Add slide images in assets folder.
    
    Replace headings with images.

--- test/subscription_insurance_test.js
+++ test/subscription_insurance_test.js
@@ -2,5 +2,5 @@ Feature('Subscription insurance page');
 
 Scenario('Show subscription insurance page', (I) => {
   I.amOnPage('/subscription-insurance?version=3');
-  I.see('Affordable price');
+  I.see('critical illness');
 });
