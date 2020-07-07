9703dc5a5be49833542eef3f4c68fb98522b0ad9
YE H <canary4651@gmail.com>
2020-07-07 16:03:44 +0900

    Set up Gatsby project
    Show version 6 web page using Gatsby.
    
    Move old page components from `pages` to `_pages` because Gatsby uses `pages`.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
-import Root from './pages/Root';
+import Root from './_pages/Root';
 
 export default function App() {
   const { t, locale } = useSelector((state) => ({
---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Link } from 'gatsby';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
