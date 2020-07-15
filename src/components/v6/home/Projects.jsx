ff7efe9fe9057c21c6cf7bbbf137647f19ee8fcc
Ashal aka JOKER <ahastudio@gmail.com>
2020-07-15 16:47:13 +0900

    Show images of the project on the home page
    Put images of the project as background to link on the home page.

--- src/components/v6/home/Projects.jsx
+++ src/components/v6/home/Projects.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import { Link, FormattedMessage } from 'gatsby-plugin-intl';
 
+import _ from 'lodash';
+
 import styled from '@emotion/styled';
 
 import { BASE_MQ } from '../../../styles/constants-v6';
+
+import { images } from '../../../assets';
 
 const NAMES = [
   'self-employed-insurance',
@@ -17,14 +21,27 @@ const List = styled.ul({
   },
 });
 
-const Item = styled.li({
+const Item = styled.li(({ image }) => ({
+  background: `#888 url(${image}) 50% 50% no-repeat`,
+  backgroundSize: 'cover',
   [BASE_MQ]: {
     width: 'calc(100% / 3)',
   },
-  '& a': {
-    display: 'block',
+}));
+
+const ItemLink = styled(Link)({
+  fontSize: '10vw',
+  display: 'flex',
+  padding: '1em',
+  width: '100%',
+  height: '80vw',
+  background: 'rgba(0, 0, 0, .5)',
+  flexDirection: 'column',
+  justifyContent: 'flex-end',
+  [BASE_MQ]: {
+    fontSize: '4vw',
+    height: '40vw',
     minHeight: '10em',
-    background: '#888',
   },
 });
 
@@ -32,10 +49,13 @@ export default function Projects() {
   return (
     <List>
       {NAMES.map((name) => (
-        <Item key={name}>
-          <Link to={`/projects/${name}`}>
+        <Item
+          key={name}
+          image={images.projects[_.camelCase(name)]}
+        >
+          <ItemLink to={`/projects/${name}`}>
             <FormattedMessage id={`home.projects.${name}`} />
-          </Link>
+          </ItemLink>
         </Item>
       ))}
     </List>
